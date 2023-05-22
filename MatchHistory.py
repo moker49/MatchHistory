6b95ff3322dc096c13d436c7165df08fdd94b33f
Efren <moker49@gmail.com>
2023-05-21 21:56:17 -0400

    refreshed

--- MatchHistory.py
+++ MatchHistory.py
@@ -1,12 +1,11 @@
 from riotwatcher import LolWatcher
 import time, json, pypyodbc as odbc
 
-
 # CONFIG 
 config = {}
 with open('config.json') as json_file:
     config = json.load(json_file)
-interval = config['interval']
+call_interval = config['call_interval']
 
 # LOR WATCHER
 api_key = config['riot_api_key']
@@ -27,6 +26,7 @@ insertProc = config['procedure_insert']
 
 print('getting matches...')
 matchIds = lol_watcher.match.matchlist_by_puuid(region, puuid, count=match_count)
+print(matchIds)
 print('matches received\n')
 
 print(f'connecting to db...')
@@ -38,7 +38,7 @@ with odbc.connect(conn_string) as con:
         print(f'getting match details {matchId} ...')
         currentMatch = lol_watcher.match.by_id(region, matchId)
         print(f'match {matchId} received')
-        info = currentMatch['info'];
+        info = currentMatch['info']
 
         for player in info['participants']:
 
@@ -60,49 +60,49 @@ with odbc.connect(conn_string) as con:
             }
 
             playerMatch = {
-                'matchId': matchId,
-                'player': player['summonerName'],
-                'gameMode': info['gameMode'],
-                'champion': player['championName'],
-                'date': info['gameStartTimestamp'],
-                'durationSec': info['gameDuration'],
-                'win': proccessed['win'],
-                'kills': player['kills'],
-                'deaths': player['deaths'],
-                'assists': player['assists'],
-                'doubleKills': player['doubleKills'],
-                'tripleKills': player['tripleKills'],
-                'quadraKills': player['quadraKills'],
-                'pentaKills': player['pentaKills'],
-                'legendaryKills': player['unrealKills'],
-                'dmgToChamps': player['totalDamageDealtToChampions'],
-                'dmgToBuildings': player['damageDealtToBuildings'],
-                'dmgTaken': player['totalDamageTaken'],
-                'dmgMitigated': player['damageSelfMitigated'],
-                'gold': player['goldEarned'],
-                'creepScore': player['totalMinionsKilled'],
-                'dragons': player['dragonKills'],
-                'barons': player['baronKills'],
-                'level': player['champLevel'],
-                'firstBlood': proccessed['firstBlood'],
-                'firstTower': proccessed['firstTower'],
-                'surrender': proccessed['surrender'],
-                'timeCCOthers': player['timeCCingOthers'],
-                'timeDead': player['totalTimeSpentDead'],
-                'crit': player['largestCriticalStrike'],
-                'spell1Cast': player['spell1Casts'],
-                'spell2Cast': player['spell2Casts'],
-                'spell3Cast': player['spell3Casts'],
-                'spell4Cast': player['spell4Casts'],
-                'summ1Cast': player['summoner1Casts'],
-                'summ2Cast': player['summoner2Casts'],
-                'summ1Id': player['summoner1Id'],
-                'summ2Id': player['summoner2Id'],
-                'wardsPlaced': player['wardsPlaced'],
-                'wardsKilled': player['wardsKilled'],
-                'puuid': player['puuid'],
+                'MATCH_ID': matchId,
+                'PLAYER': player['summonerName'],
+                'GAME_MDOE': info['gameMode'],
+                'CHAMPION': player['championName'],
+                'DATE': info['gameStartTimestamp'],
+                'DURATION': info['gameDuration'],
+                'WIN': proccessed['win'],
+                'KILLS': player['kills'],
+                'DEATHS': player['deaths'],
+                'ASSISTS': player['assists'],
+                'DOUBLE_KILLS': player['doubleKills'],
+                'TRIPLE_KILLS': player['tripleKills'],
+                'QUADRA_KILLS': player['quadraKills'],
+                'PENTA_KILLS': player['pentaKills'],
+                'LEGENDARY_KILLS': player['unrealKills'],
+                'DMG_TO_CHAMPS': player['totalDamageDealtToChampions'],
+                'DMG_TO_STRUCT': player['damageDealtToBuildings'],
+                'DMG_TAKEN': player['totalDamageTaken'],
+                'DMG_MITIGATED': player['damageSelfMitigated'],
+                'GOLD': player['goldEarned'],
+                'CREEP_SCORE': player['totalMinionsKilled'],
+                'DRAGONS': player['dragonKills'],
+                'BARONS': player['baronKills'],
+                'LEVEL': player['champLevel'],
+                'FIRST_BLOOD': proccessed['firstBlood'],
+                'FIRST_TOWER': proccessed['firstTower'],
+                'SURRENDER': proccessed['surrender'],
+                'TIME_CC_OTHER': player['timeCCingOthers'],
+                'TIME_DEAD': player['totalTimeSpentDead'],
+                'CRIT': player['largestCriticalStrike'],
+                'SPELL_1_CAST': player['spell1Casts'],
+                'SPELL_2_CAST': player['spell2Casts'],
+                'SPELL_3_CAST': player['spell3Casts'],
+                'SPELL_4_CAST': player['spell4Casts'],
+                'SUMM_1_CAST': player['summoner1Casts'],
+                'SUMM_2_CAST': player['summoner2Casts'],
+                'SUMM_1_ID': player['summoner1Id'],
+                'SUMM_2_ID': player['summoner2Id'],
+                'WARDS_PLACED': player['wardsPlaced'],
+                'WARDS_KILLED': player['wardsKilled'],
+                'PUUID': player['puuid'],
             }
-            champName = player['championName']
+            playerName = player['summonerName']
             # print(playerMatch)
 
             procParamKeys = '@' + ' = ?, @'.join(playerMatch.keys()) + ' = ?'
@@ -110,10 +110,10 @@ with odbc.connect(conn_string) as con:
             SQL = f'EXEC {insertProc} {procParamKeys};'
 
             cursor.execute(SQL, procParamValues)
-            print(f'inserted: {matchId}/{champName}...')
+            print(f'inserted: {matchId}/{playerName}...')
        
-        print(f'waiting: {interval}s...\n')
-        time.sleep(interval)
+        print(f'waiting: {call_interval}s...\n')
+        time.sleep(call_interval)
     while cursor.nextset(): pass
     cursor.commit()
 
